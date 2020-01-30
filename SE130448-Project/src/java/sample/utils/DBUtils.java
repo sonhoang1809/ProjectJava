85332220c2da1a69f37de35f1f497599dc4587ae
sh <sonhoang1809@gmail.com>
2020-01-31 00:31:09 +0700

    Project - Sell Home
    Project Sell Home

---
+++ SE130448-Project/src/java/sample/utils/DBUtils.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author sonho
+ */
+public class DBUtils {
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String url = "jdbc:sqlserver://sonhoang.database.windows.net:1433;databaseName=ProjectWeb";
+        return DriverManager.getConnection(url, "sonhoang", "Sondep1809");
+    }
+}
